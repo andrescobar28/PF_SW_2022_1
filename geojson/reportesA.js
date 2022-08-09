d0d4696e13af29e2f28a627a0f8f5be6b4989a11
Andrés Escobar <andres.julian.escobar@correounivalle.edu.co>
2022-08-09 02:52:50 -0500

    v3

---
+++ geojson/reportesA.js
@@ -0,0 +1,8 @@
+var reportesA = {
+"type": "FeatureCollection",
+"name": "reportes",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "21", "direccion": "Carrera 66 Calle 9", "latitud_x": "3.39831268698603", "longitud_y": "-76.54446670336668", "tipo_repor": "Fisuras", "descripcio": "Grieta grande en la via", "fecha": "09\/08\/2022", "hora": "10:00 am", "intensidad": "Muy Malo" }, "geometry": { "type": "Point", "coordinates": [ -76.54446670336668, 3.39831268698603 ] } }
+]
+}
---
+++ geojson/reportesC.js
@@ -0,0 +1,8 @@
+var reportesC = {
+"type": "FeatureCollection",
+"name": "reportes",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "22", "direccion": "Calle 10 Carrera 39", "latitud_x": "3.4211834598743014", "longitud_y": "-76.53542395918659", "tipo_repor": "Daños superficiales", "descripcio": "Mal estado de pavimento", "fecha": "09\/08\/2022", "hora": "10:10 am", "intensidad": "Regular" }, "geometry": { "type": "Point", "coordinates": [ -76.53542395918659, 3.4211834598743014 ] } }
+]
+}
